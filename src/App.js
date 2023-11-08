f36fa29efd4f7ad3fc00c3954b4cab1958988b02
Martyna <martyna@Rogerios-MacBook-Pro.local>
2023-11-08 11:19:41 -0500

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,19 @@
-import logo from './logo.svg';
+
 import './App.css';
+import { useDispatch, useSelector } from 'react-redux';
+import { increment, decrement, incrementByAmount } from './redux/counter';
 
 function App() {
+
+  const { count } = useSelector((state) => state.counter)
+  const dispatch = useDispatch();
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>The count is: {count} </h1>
+      <button onClick={() =>dispatch(decrement())}>decrement</button>
+      <button onClick={() =>dispatch(increment())}>increment</button>
+    <button onClick={()=> dispatch(incrementByAmount(33))}>Increment by 33</button>
     </div>
   );
 }
